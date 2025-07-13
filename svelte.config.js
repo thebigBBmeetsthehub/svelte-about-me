55f8a8263adc96d264a2a4bde1e046b607413bde
thebigBBmeetsthehub <briarlynnbryant@gmail.com>
2025-07-13 10:46:48 -0700

    Update svelte.config.js to try to fix display error

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,21 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import adapter from '@sveltejs/adapter-static'; 
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-export default {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
-  preprocess: vitePreprocess(),
-}
+const dev = process.argv.includes('dev'); 
+
+const config = {
+    preprocess: vitePreprocess(),
+
+    kit: {
+        adapter: adapter({
+        }),
+        paths: {
+            base: dev ? '' : process.env.BASE_PATH || '/svelte-about-me',
+        },
+        prerender: {
+            entries: ['*', '/404'] 
+        },
+    }
+};
+
+export default config;
